9819a6887c5e60247c24c38c84dfc174600e1518
Claúdio Junior <99290044+claudiojuniorr@users.noreply.github.com>
2023-03-13 09:37:56 -0300

    feat:button-add-updated

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,53 @@
-gi
+const cardTitulodRef = document.querySelector('#cardTitulo')
+const cardImgRef = document.querySelector('#carImg')
+const cardTextdRef = document.querySelector('#cardText')
+cardTextdRef.value =''
+const cardButtonRef = document.querySelector('#buttonCreate')
+var containerCardElement = document.querySelector('.card-all')
+
+const card = {
+    titulo: '',
+    url: '',
+    texto: ''
+}
+
+const cards = []
+var lengthCards = cards.length
+
+function cardTitulo(titulo){
+    card.titulo = titulo
+}
+
+function cardImg(img){
+    card.url = img
+}
+
+function cardText(text){
+    card.texto = text
+}
+
+function btnCreate(event){
+    event.preventDefault()
+    cards.push(card)
+    cardTitulodRef.value =''
+    cardImgRef.value =''
+    cardTextdRef.value =''
+    containerCardElement.innerHTML += `
+    <div class="card">
+        <img src="${cards[lengthCards].url}">
+        <header class="header-card">
+            <h1 class="titulo-card">${cards[lengthCards].titulo}</h1>
+        </header>
+        <main class="main-card">
+            <p>${cards[lengthCards].texto}</p>
+        </main>
+    </div>
+    `
+    let child = document.querySelector('.card-all p')
+    containerCardElement.removeChild(child)
+}
+
+cardTitulodRef.addEventListener('keyup', (event) => cardTitulo(event.target.value))
+cardImgRef.addEventListener('keyup', (event) => cardImg(event.target.value))
+cardTextdRef.addEventListener('keyup', (event) => cardText(event.target.value))
+cardButtonRef.addEventListener('click', (event) => btnCreate(event))
